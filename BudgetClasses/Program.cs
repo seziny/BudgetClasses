8874ab623a10a00f8a3dac1e2a8dd1dcb2764d4e
seziny <sezinyildiz@gmail.com>
2015-10-04 20:02:21 +0200

    First version
    First commit of the project

---
+++ BudgetClasses/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BudgetClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CurrencyType test = CurrencyType.EUR;
+
+            Console.WriteLine(test.ToString());
+
+            Console.WriteLine("Done.");
+            Console.ReadKey();
+        }
+    }
+}
