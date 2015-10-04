d17060baf166d0eae76135b4470c328597ed8b06
seziny <sezinyildiz@gmail.com>
2015-10-04 19:54:22 +0200

    First version
    First version of our accounting system

---
+++ BudgetClasses/Entry.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace BudgetClasses
+{
+    // A zero-sum list of withdrawals or deposits to various accounts, 
+    // and the associated import data.
+    class Transaction
+    {
+        DateTime Time;
+
+        Entry[]  Entries;
+
+        string Description;
+        string[] Tags;
+
+        string ImportData;
+    }
+
+    // Entry
+    class Entry
+    {
+        Money Value;
+        string Account;
+        string Allocation;
+    }
+
+    // A sum of money and currency
+    class Money
+    {
+        long         Cents;
+        CurrencyType Currency;
+
+    }
+
+}
